f85ec4118867da8ec52b56ea584d3f579832170a
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-10 19:42:40 +0900

    Update 하노이의 탑_인터넷.py

--- WEEK01/하노이의 탑_인터넷.py
+++ WEEK01/하노이의 탑_인터넷.py
@@ -1,15 +1,12 @@
 answer = []
 def dfs(n, x, y):
     global answer
-    if n==2:
-        answer.append([x,6-x-y])
+    if n==1:
         answer.append([x,y])
-        answer.append([6-x-y,y])
     else:
         dfs(n-1, x, 6-x-y)
         answer.append([x,y])
         dfs(n-1, 6-x-y, y)
 def solution(n):
-    if n==1: return [1,3]
     dfs(n, 1, 3)
     return answer
