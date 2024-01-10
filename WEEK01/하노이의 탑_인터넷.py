008b9213dfa1014b6b68b5dc1784e1be227191bd
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-10 19:46:50 +0900

    Update 하노이의 탑_인터넷.py

--- WEEK01/하노이의 탑_인터넷.py
+++ WEEK01/하노이의 탑_인터넷.py
@@ -10,3 +10,20 @@ def dfs(n, x, y):
 def solution(n):
     dfs(n, 1, 3)
     return answer
+
+
+
+
+def solution(n):
+    answer = []
+    def dfs(n, src, tgt, inter):
+        if n==1:
+            answer.append([src,tgt])
+        else:
+            dfs(n-1, src, inter, tgt)
+            answer.append([src,tgt])
+            dfs(n-1, inter, tgt, src)    
+    dfs(n, 1, 3, 2)
+    return answer
+    
+    
